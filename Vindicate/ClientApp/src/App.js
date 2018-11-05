be40192e176b794170d05e6aa02fb0114a05be5a
Daniel Davis <35809731+danielbls@users.noreply.github.com>
2018-11-05 02:27:17 -0500

    Organized Folders

--- Vindicate/ClientApp/src/App.js
+++ Vindicate/ClientApp/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router';
 import { Layout } from './components/Layout';
-import { Home } from './components/Home';
 import { Counter } from './components/Counter';
 import ProjectList from './components/Project/ProjectList';
 import ProjectDetails from './components/Project/ProjectDetails';
