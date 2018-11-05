e30842a821a833a370c7e1809987507a35334dff
Daniel Davis <35809731+danielbls@users.noreply.github.com>
2018-11-05 02:24:56 -0500

    Menu Router Updates

--- Vindicate/ClientApp/src/components/NavMenu.js
+++ Vindicate/ClientApp/src/components/NavMenu.js
@@ -1,11 +1,17 @@
 ﻿import React from 'react';
+import { Redirect } from 'react-router';
+import { withRouter } from 'react-router-dom';
 import { Layout, Menu, Icon } from 'antd';
 import "antd/dist/antd.css";
 import "./NavMenu.css";
 
 const { Sider } = Layout;
 
-export default class NavMenu extends React.Component {
+class NavMenu extends React.Component {
+    handleClick = (e) => {
+        this.props.history.push(e.item.props.url);
+    }
+
     render() {
         return (
             <Sider style={{ overflow: 'auto', height: '100vh', maxWidth: 'unset !important', position: 'absolute', left: 0 }}
@@ -16,23 +22,23 @@ export default class NavMenu extends React.Component {
                     onCollapse={(collapsed, type) => { console.log(collapsed, type); }}
                 >
                     <div className="logo" />
-                <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
+                <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']} onClick={this.handleClick}>
                     <Menu.Item key="0">
                         <h3 style={{ color: "#ffffff", marginBottom: "none", marginTop: "10px" }}>Vindicate</h3>
                         </Menu.Item>
-                        <Menu.Item key="1">
+                        <Menu.Item key="1" url="projects">
                             <Icon type="pushpin" />
                             <span className="nav-text">Projects</span>
                         </Menu.Item>
-                        <Menu.Item key="2">
+                        <Menu.Item key="2" url="issues">
                             <Icon type="info-circle" />
                             <span className="nav-text">Issues</span>
                         </Menu.Item>
-                        <Menu.Item key="3">
+                        <Menu.Item key="3" url="reports">
                             <Icon type="file" />
                             <span className="nav-text">Reports</span>
                         </Menu.Item>
-                        <Menu.Item key="4">
+                        <Menu.Item key="4" url="admin">
                             <Icon type="setting" />
                             <span className="nav-text">Administration</span>
                         </Menu.Item>
@@ -40,3 +46,5 @@ export default class NavMenu extends React.Component {
                 </Sider>);
     }
 }
+
+export default withRouter(NavMenu)
